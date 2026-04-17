671fba634e731b7afe44f7dc83f87ebdb338bfed
Ben Chen <idben.chen@gmail.com>
2026-04-17 09:39:44 +0800

    card01

---
+++ card01/src/Bs.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+<template>
+    <div class="container mt-3">
+        <div class="d-flex justify-content-center">
+            <!-- <div class="card w400px mh300px">
+                <img src="https://images.pexels.com/photos/531844/pexels-photo-531844.jpeg" alt="" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        計算機並不一切及時有很多小
+                    </h5>
+                    <p>售價爭取現代批評簡單種子網易持續適應，答應是個趕緊美國禁止控制據說地上具備傳統我一晶片，資料庫，這。</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between bg-white border-top-0">
+                    <button class="btn btn-primary">上一個</button>
+                    <button class="btn btn-primary">下一個</button>
+                </div>
+            </div> -->
+            <div class="card w400px mh300px">
+                <img src="https://images.pexels.com/photos/531844/pexels-photo-531844.jpeg" alt="" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        計算機並不一切及時有很多小
+                    </h5>
+                    <p>售價爭取現代批評簡單種子網易持續適應，答應是個趕緊美國禁止控制據說地上具備傳統我一晶片，資料庫，這。</p>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-info me-1">上一個</button>
+                    <button type="button" class="btn btn-info">下一個</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.w400px {
+    width: 400px;
+}
+
+.mh300px {
+    min-height: 300px;
+}
+</style>
---
+++ card01/src/main.js
@@ -0,0 +1,6 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './Bs.vue'
+
+createApp(App).mount('#app')
